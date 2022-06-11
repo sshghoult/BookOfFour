c4ba19e4602d77147ec910fd6bb9b42adc6e95dc
sshghoult <50514492+sshghoult@users.noreply.github.com>
2022-06-11 08:33:10 +0700

    Abstract Fabric: Plane

--- Creational Patterns/AbstractFactory/Plane.py
+++ Creational Patterns/AbstractFactory/Plane.py
@@ -1,13 +1,58 @@
+from datetime import timedelta
+
 from .Abstract import AbstractDeliveryFactory, AbstractTransport, AbstractPackageRestrictions, AbstractPriceCalculator
-from typing import Type
+from typing import Type, Collection, Tuple
+
+
+class Plane(AbstractTransport):
+
+    @property
+    def volume(self) -> int:
+        return 10 ** 5
+
+    @property
+    def delivery_time(self) -> timedelta:
+        return timedelta(days=1)
+
+
+class PlanePackageRestrictions(AbstractPackageRestrictions):
+
+    @property
+    def size(self) -> Tuple[int, int, int]:
+        return 1000, 1000, 450
+
+    @property
+    def prohibited_goods(self) -> Collection:
+        return {'Manicure Scissors', 'Duty Free Scotch'}
+
+
+class PlanePriceCalculator(AbstractPriceCalculator):
+
+    def __init__(self, size, weight):
+        super().__init__()
+        self._size = size
+        self._volume = size[0] * size[1] * size[2]
+
+        self._weight = weight
+
+        self._insurance = None
+
+        self._size_policy = 0.7
+        self._weight_policy = 0.9
+
+    def add_insurance(self, insurance_policy: 'AbstractInsurancePolicy'):
+        self._insurance = insurance_policy
+
+    def get_price(self):
+        return max(self._size_policy * self._volume, self._weight_policy * self._weight) + self._insurance.get_price()
+
 
 class PlaneDeliveryFactory(AbstractDeliveryFactory):
-    def get_transport(self):
-        pass
+    def get_transport(self) -> AbstractTransport:
+        return Plane()
 
-    def get_package_restrictions(self):
-        pass
+    def get_package_restrictions(self) -> AbstractPackageRestrictions:
+        return PlanePackageRestrictions()
 
-    def get_price_calculator(self):
-        pass
-
+    def get_price_calculator(self) -> Type[AbstractPriceCalculator]:
+        return PlanePriceCalculator
